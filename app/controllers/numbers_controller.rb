dba85e7ed4c6829b31df8d3da65b4fdaf16b510d
Camille Arce <thecamillearce@gmail.com>
2023-03-29 20:53:41 +0000

    debugged fire, air, and earth signs

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,20 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @unluckynumbers = Array.new
+    5.times do
+      giraffe = rand(1...100)
+
+      @unluckynumbers.push(giraffe)
+    end
+    render({ :template => "lottery_stuff/notwoohoo.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   # PART 1: EACH IN ERB
   # ===================
 
+  #Camille's Notes: This app uses :controller => "numbers" instead of "application" because it is using a custom :controller... it works the same because the custom controller "numbers" is inheriting ApplicationController under the ApplicationController files --- like this
+  # as seen under app/controllers/numbers_controller.rb
+  # class NumbersController < ApplicationController
+
+  # Camille's notes: Part 1 of the project: implement /lottery/unlucky
   get("/lottery/lucky", { :controller => "numbers", :action => "winners" })
-  get("/", { :controller => "numbers", :action => "winners" })
+  get("/lottery/unlucky", { :controller => "numbers", :action => "losers" })
 
   # Let users visit URLs:
-
   # - /lottery/unlucky
 
   # PART 2: R→C→A→V DEBUGGING
@@ -15,25 +19,25 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
   # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
 
-  # get("/zodiacs/aries", { :controller =>  fire, action =>  "ram" })
-  # get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  # get("/zodiacs/sagittarius" { :controller => "fire", :action => "archer" })
-  
-  # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
-  
-  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
-  
+  get("/zodiacs/aries", { :controller => "fires", :action => "ram" })
+  get("/zodiacs/leo", { :controller => "fires", :action => "lion" })
+  get("/zodiacs/sagittarius", { :controller => "fires", :action => "archer" })
+
+  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+
   # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
   # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
   # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
-  
+
   # Let users visit the following URLs.
   # See the target to find out what the interface should be — then match it.
   # Follow the standard R→C→A→V procedure:
